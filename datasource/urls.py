a467f78480882ef12f70ee36f36e2dd65363ee33
Jim Mathew <jimmat53@yahoo.co.in>
2017-07-13 14:44:35 -0700

    Minor corrections.

--- datasource/urls.py
+++ datasource/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import views
+from datasource import views
 
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
